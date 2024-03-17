dfee52a080c6f2dd49850637916bbc33601ca154
tiago863 <tiagopedro02.iscte@gmail.com>
2024-03-17 23:01:47 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-const btn_nav = document.getElementById("botao-container");
-const nav_secundaria = document.getElementById("nav_secundaria");
-const slide_show_container = document.getElementById("slide-show_container");
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
 
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
 
-btn_nav.addEventListener("click", function () {
-    if (nav_secundaria.classList.contains("expanded")) {
-        nav_secundaria.classList.add("slide-out")
-        nav_secundaria.classList.remove("expanded");
-        slide_show_container.style.marginTop = "0";
-    } else {
-        nav_secundaria.classList.remove("slide-out");
-        nav_secundaria.classList.add("expanded");
-        slide_show_container.style.marginTop = nav_secundaria.clientHeight + "px";
-    }
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
 });
